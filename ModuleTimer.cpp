16ac49b95ee3454d3c7f3e03c7ca62c72fa38d01
joey3553 <pep3553@gmail.com>
2015-04-16 18:49:31 +0200

    Timer vuelve

---
+++ ModuleTimer.cpp
@@ -0,0 +1,47 @@
+#include "Globals.h"
+#include "Application.h"
+#include "ModuleTimer.h"
+
+ModuleTimer::ModuleTimer(Application* app, bool start_enabled) : Module(app, start_enabled)
+{
+	graphics = NULL;
+
+	//Status bar, animation
+	status.frames.PushBack({ 412, 77, 256, 32 });
+	current_status = &status;
+
+}
+
+ModuleTimer::~ModuleTimer()
+{}
+
+// Load assets
+bool ModuleTimer::Start()
+{
+	LOG("Loading scene");
+
+	graphics = App->textures->Load("Game/GameAssets.png");
+	App->scene->Enable();
+
+	return true;
+}
+
+// UnLoad assets
+bool ModuleTimer::CleanUp()
+{
+	LOG("Unloading space scene");
+
+	App->textures->Unload(graphics);
+	App->scene->Disable();
+
+	return true;
+}
+
+// Update: draw background
+update_status ModuleTimer::Update()
+{
+	// Draw everything --------------------------------------
+	App->renderer->Blit(graphics, 0, 0, &(current_status->GetCurrentFrame()));
+
+	return UPDATE_CONTINUE;
+}
---
+++ ModuleTimer.h
@@ -0,0 +1,24 @@
+#pragma once
+
+#include "Module.h"
+#include "Animation.h"
+#include "Globals.h"
+
+class ModuleTimer : public Module
+{
+public:
+	ModuleTimer(Application* app, bool start_enabled = true);
+	~ModuleTimer();
+
+	bool Start();
+	update_status Update();
+	bool CleanUp();
+
+public:
+
+	SDL_Texture* graphics;
+	Animation* current_status;
+	Animation status;
+
+
+};
