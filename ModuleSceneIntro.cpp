be190f03268048b7e0f3b5666ce7b37ee673bafa
traguill <traguill1@gmail.com>
2015-05-20 20:36:01 +0200

    Menu completed (not ready)

--- ModuleSceneIntro.cpp
+++ ModuleSceneIntro.cpp
@@ -8,10 +8,15 @@ ModuleSceneIntro::ModuleSceneIntro(Application* app, bool start_enabled) : Modul
 {
 	background = NULL;
 	planes = NULL;
+	text = NULL;
 
 	zepelin = { 75, 0, 32, 19};
 	ballon = { 80, 47, 44, 77};
 	zepelin_fire = { 0, 0, 73, 41 };
+
+	text_up = {0,0,255,66};
+
+	text_down = {0, 66, 255, 85};
 
 	fx = 0;
 }
@@ -28,6 +33,7 @@ bool ModuleSceneIntro::Start()
 
 	background = App->textures->Load("buildings.png");
 	planes = App->textures->Load("planes.png");
+	text = App->textures->Load("title.png");
 
 	App->audio->PlayMusic("TitleMusic.ogg", 0.0f);
 
@@ -44,6 +50,18 @@ bool ModuleSceneIntro::Start()
 	ballon_position.x = 5;
 	ballon_position.y = 10;
 
+	//Text
+	text_up_position.x = -256;
+	text_up_position.y = 0;
+
+	text_down_position.x = 257;
+	text_down_position.y = 66;
+
+	//Variables
+	flash = 0;
+	start_time = SDL_GetTicks();
+
+
 	return ret;
 }
 
@@ -54,16 +72,43 @@ bool ModuleSceneIntro::CleanUp()
 
 	App->textures->Unload(background);
 	App->textures->Unload(planes);
+	App->textures->Unload(text);
 
 	return true;
 }
 
 update_status ModuleSceneIntro::Update()
 {
+	actual_time = SDL_GetTicks() - start_time;
+
 	zepelin_position.x += 0.3f;
 	zepelin_fire_position.x -= 0.8f;
 	ballon_position.x += 0.4f;
 	ballon_position.y += sin(0.003f*SDL_GetTicks())*0.3f;
+
+	//Text apears after 3s---------------------------------------
+	if (actual_time > 3000) 
+	{
+		if (text_up_position.x < 0)
+		{
+			text_up_position.x += 8;
+			text_down_position.x -= 8;
+		}
+		else
+		{
+			
+			if (flash == 0)
+			{
+				flash = 1;
+				flash_startime = actual_time;
+			}
+
+			text_up_position.x = 0;
+			text_down_position.x = 0;
+		}
+	}
+	
+	
 	
 
 	// Draw background --------------------------------------	
@@ -74,6 +119,12 @@ update_status ModuleSceneIntro::Update()
 	App->renderer->Blit(planes, zepelin_fire_position.x, zepelin_fire_position.y, &zepelin_fire);
 	App->renderer->Blit(planes, ballon_position.x, ballon_position.y, &ballon);
 
+	//Draw text------------------------------------------------
+	App->renderer->Blit(text, text_up_position.x, text_up_position.y, &text_up);
+	App->renderer->Blit(text, text_down_position.x, text_down_position.y, &text_down);
+
+	if (flash == 1 && actual_time - 50 <= flash_startime)
+		App->renderer->DrawQuad({0,0,1000,1000}, 255, 255, 255, 255, false);
 
 
 	//Change scene--------------------------------------------
--- ModuleSceneIntro.h
+++ ModuleSceneIntro.h
@@ -17,14 +17,27 @@ public:
 
 	SDL_Texture* background;
 	SDL_Texture* planes;
+	SDL_Texture* text;
 
 	SDL_Rect ballon;
 	SDL_Rect zepelin;
 	SDL_Rect zepelin_fire;
 
+	SDL_Rect text_up;
+	SDL_Rect text_down;
+
 	p2Point<float> ballon_position;
 	p2Point<float> zepelin_position;
 	p2Point<float> zepelin_fire_position;
+
+	p2Point<float> text_up_position;
+	p2Point<float> text_down_position;
+
+	int flash; //0- No 1-Need to flash 2-Has flashed
+	Uint32 flash_startime;
+
+	Uint32 start_time;
+	Uint32 actual_time;
 
 	unsigned int fx;
 };
