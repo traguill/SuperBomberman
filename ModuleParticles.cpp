44469dee6e673a53f87f5a183fe39d66dd95dfe0
joeyGumer <pep3553@gmail.com>
2015-06-12 01:53:49 +0200

    SERSO

--- ModuleParticles.cpp
+++ ModuleParticles.cpp
@@ -6,11 +6,6 @@
 ModuleParticles::ModuleParticles(Application* app, bool start_enabled) : Module(app, start_enabled), graphics(NULL)
 {
 
-<<<<<<< HEAD
-=======
-	
->>>>>>> origin/master
-
 	graphics = NULL;
 
 
